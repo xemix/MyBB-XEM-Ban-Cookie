b4f11e220f6a71bb44f7ea4e2057af30c4757624
Xemix <szczepan@xemix.eu>
2015-03-10 17:20:52 +0100

    Aktualizacja do wersji 1.2
    - uporządkowanie kodu (przepisanie funkcji na klasę)
    - dodanie kompatybilności z PHP 5.3

--- inc/plugins/xem_bancookie.php
+++ inc/plugins/xem_bancookie.php
@@ -7,66 +7,63 @@
  * License URL: http://creativecommons.org/licenses/by-nc-sa/4.0/
  */
 
-if(!defined("IN_MYBB"))
-{
-    die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
-}
+if(!defined("IN_MYBB")) exit();
 
-$plugins->add_hook("no_permission", "xem_bancookie_setcookie");
-$plugins->add_hook("member_register_agreement", "xem_bancookie_check");
-$plugins->add_hook("member_logout_start", "xem_bancookie_disable_logout");
+$plugins -> add_hook('no_permission', array('xem_bancookie', 'set_cookie'));
+$plugins -> add_hook('member_register_agreement', array('xem_bancookie', 'disable_register'));
+$plugins -> add_hook('member_logout_start', array('xem_bancookie', 'disable_logout'));
 
 function xem_bancookie_info()
 {
-    return [
+    return array(
         'name'          => 'xemBanCookie',
-        'description'   => 'The plugin creates a cookie, whereby banned users cannot register again or logout.',
+        'description'   => 'This plugin creates a cookie, whereby banned users cannot register again or logout.',
         'website'       => 'http://xemix.eu',
-        'author'        => 'Xemix.eu',
+        'author'        => 'Xemix',
         'authorsite'    => 'http://xemix.eu',
-        'version'       => '1.1',
+        'version'       => '1.2',
         'codename'      => 'xem_bancookie',
         'compatibility' => '18*'
-    ];
+    );
 }
 
 function xem_bancookie_install()
 {
     global $db, $mybb;
 
-    $settingGroupId = $db->insert_query('settinggroups', [
+    $setting_group_id = $db->insert_query('settinggroups', array(
         'name'        => 'xem_bancookie_settings',
         'title'       => 'xemBanCookie settings',
-        'description' => 'Settings for BanCookie.',
-    ]);
+        'description' => 'Settings for xemBanCookie.',
+    ));
     
-    $settings = [
-        [   
+    $settings = array(
+        array(   
             'name'        => 'xem_bancookie_active',
             'title'       => 'xemBanCookie active',
             'description' => 'The BanCookie plugin is activated?',
             'optionscode' => 'yesno',
             'value'       => '1'
-        ],
-        [   
+        ),
+        array(  
             'name'        => 'xem_bancookie_disable_logout',
             'title'       => 'Block the logout after ban?',
             'description' => 'Users cannot logout when the account has been banned.',
             'optionscode' => 'yesno',
             'value'       => '1'
-        ],
-    ];
+        ),
+    );
 
     $i = 1;
 
     foreach($settings as &$row) {
-        $row['gid']         = $settingGroupId;
-        $row['title']       = $db->escape_string($row['title']);
-        $row['description'] = $db->escape_string($row['description']);
+        $row['gid']         = $setting_group_id;
+        $row['title']       = $db -> escape_string($row['title']);
+        $row['description'] = $db -> escape_string($row['description']);
         $row['disporder']   = $i++;
     }
 
-    $db->insert_query_multiple('settings', $settings);
+    $db -> insert_query_multiple('settings', $settings);
 
     rebuild_settings();
     
@@ -76,13 +73,13 @@ function xem_bancookie_uninstall()
 {
     global $db;
 
-    $settingGroupId = $db->fetch_field(
-        $db->simple_select('settinggroups', 'gid', "name='xem_bancookie_settings'"),
+    $setting_group_id = $db -> fetch_field(
+        $db -> simple_select('settinggroups', 'gid', "name='xem_bancookie_settings'"),
         'gid'
     );
 
-    $db->delete_query('settinggroups', "name='xem_bancookie_settings'");
-    $db->delete_query('settings', 'gid=' . $settingGroupId);
+    $db -> delete_query('settinggroups', "name='xem_bancookie_settings'");
+    $db -> delete_query('settings', 'gid=' . $setting_group_id);
 
     rebuild_settings();
 }
@@ -91,45 +88,60 @@ function xem_bancookie_is_installed()
 {
     global $db;
 
-    $query = $db->simple_select('settinggroups', 'gid', "name='xem_bancookie_settings'");
-    return (bool)$db->num_rows($query);
+    $query = $db -> simple_select('settinggroups', 'gid', "name='xem_bancookie_settings'");
+    return (bool)$db -> num_rows($query);
 }
 
-function xem_bancookie_setcookie()
+class xem_bancookie 
 {
-    global $mybb, $db;
 
-    if(xem_bancookie_is_installed() && 
-      (int)$mybb->settings['xem_bancookie_active'] &&
-       $mybb->usergroup['isbannedgroup'])
+    public function set_cookie()
     {
-        $query = $db->simple_select('banned', '*', "uid = '{$mybb->user['uid']}'", ['limit' => 1]);
-        $ban = $db->fetch_array($query);
-        $expire = $ban['lifted'] - TIME_NOW;
-        my_setcookie("mybb[userban]", md5($ban['lifted']), $expire);
+        global $mybb, $db;
+
+        if((int)$mybb -> settings['xem_bancookie_active'] && $mybb -> usergroup['isbannedgroup'])
+        {
+            $query = $db -> simple_select('banned', 'lifted', "uid = '{$mybb->user['uid']}'", array('limit' => 1));
+            $ban = $db -> fetch_array($query);
+            my_setcookie("mybb[userban]", md5($ban['lifted']), ($ban['lifted'] - TIME_NOW));
+        }
     }
-}
 
-function xem_bancookie_check()
-{
-    global $mybb, $lang;
-
-    $lang->load("xem_bancookie");
-
-    if($mybb->cookies['mybb']['userban'] && (int)$mybb->settings['xem_bancookie_active'])
+    public function disable_register()
     {
-        error($lang->xem_bancookie_user_banned);
+        global $lang;
+
+        $lang -> load("xem_bancookie");
+
+        if(self::isset_cookie('xem_bancookie_active'))
+        {
+            error($lang -> xem_bancookie_user_banned);
+        }
     }
-}
 
-function xem_bancookie_disable_logout()
-{
-    global $mybb, $lang;
-
-    $lang->load("xem_bancookie");
-
-    if($mybb->cookies['mybb']['userban'] && (int)$mybb->settings['xem_bancookie_disable_logout'])
+    public function disable_logout()
     {
-        error($lang->xem_bancookie_logout_disabled);
+        global $lang;
+
+        $lang -> load("xem_bancookie");
+
+        if(self::isset_cookie('xem_bancookie_disable_logout'))
+        {
+            error($lang -> xem_bancookie_logout_disabled);
+        }
+    }
+
+    static function isset_cookie($xem_bancookie_setting)
+    {
+        global $mybb;
+
+        if($mybb -> cookies['mybb']['userban'] && (int)$mybb -> settings[$xem_bancookie_setting]) 
+        {
+            return true;
+        } 
+        else 
+        {
+            return false;
+        }
     }
 }
